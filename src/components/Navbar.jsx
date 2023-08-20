ba508cfdab96bdb298e6264c61657d6bc8a0838c
Ali Asım Bulut <aliasim1625@hotmail.com>
2023-08-20 20:53:15 +0300

    Navbar toggler düzeltildi

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -51,10 +51,10 @@ function Navbar(props) {
             <a class="navbar-brand" href="#" id="giopods" >GioPods</a>
 
           </div>
+          <div className="right">
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div className="right">
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
               <div class="navbar-nav ul ">
                     <Link  to="/"> 
