8aba805cd0ac6cb516d2954196111f23b7c8355f
Ali Asım Bulut <aliasim1625@hotmail.com>
2023-08-23 17:42:28 +0300

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,50 +1,9 @@
-import "../css/Navbar.css"
 import { Link } from "react-router-dom";
+import "../Css/Navbar.css"
 
 function Navbar(props) {
   return (
     <>
-      {/* <div className="container navbar_container">
-        <div className="row navbar_row">
-          <div className="col-sm navbar_col">
-          <div className=" left_side">
-            <ul>
-              <li>Giopods</li>
-            </ul>
-          </div>
-          <div className=" right_side">
-              <nav>
-                <ul>
-                  <Link  to="/"> 
-                  <li> <a className="nav-link" id="home"  href="#">Home</a> </li>
-                  </Link>
-                  <Link  to="/explore" >
-                    <li> <a id="about" href="#">About</a> </li>
-                  </Link>
-                  <Link  to="/creating" >
-                    <li> <a id="channel" href="#">Channel</a> </li>
-                  </Link>
-                  <Link  to="/episodes">
-                    <li> <a id="episodes" href="#">Episodes</a> </li>
-                  </Link>
-                  <Link  to="/blog">
-                    <li> <a id="blog" href="#">Products</a> </li>
-                  </Link>
-                  <Link  to="/basket">
-                    <li> <a id="basket" href="#">Sepet</a> </li>  
-                  </Link>
-                  <Link to="/sign" >
-                      <li> <a id="sign" href="#">Kayıt Ol</a> </li>
-                  </Link>
-                  
-                </ul>
-              </nav>
-          </div>
-
-          </div>
-        </div>
-      </div> */}
-
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid navbar_container ">
           <div className="left">
